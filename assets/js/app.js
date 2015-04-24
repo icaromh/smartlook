933f591d0052f703a75db03fc92445ccd3ef976c
icaromh <icaro@icaromh.com>
2015-04-24 18:10:07 -0300

    Adicionadotudo

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,15 +5,15 @@ angular
     ]
   )
 
-  .config([
-    '$locationProvider'
-    , function($locationProvider){
-      $locationProvider.html5Mode({
-        enabled: true,
-        requireBase: true
-      });
-    }
-  ])
+  // .config([
+  //   '$locationProvider'
+  //   , function($locationProvider){
+  //     $locationProvider.html5Mode({
+  //       enabled: true,
+  //       requireBase: true
+  //     });
+  //   }
+  // ])
 
   .config(function($routeProvider){
 
@@ -29,6 +29,10 @@ angular
         templateUrl: 'assets/templates/login.html'
       })
 
+      .when('/look', {
+        controller: 'LookController',
+        templateUrl: 'assets/templates/look.html'
+      })
 
       .otherwise({
         redirectTo : '/'
@@ -38,7 +42,9 @@ angular
 
   .controller('AppCtrl', ['$scope', '$mdSidenav', '$location',
     function($scope, $mdSidenav, $location){
-
+      $scope.goInicio = function(){
+        $location.path('/app');
+      }
     }
   ])
 
@@ -46,7 +52,6 @@ angular
     function($scope, $mdSidenav, $location, $rootScope){
 
       setTimeout(function(){
-        console.log('oi');
         $rootScope.$apply(function() {
          $location.path('app');
         });
@@ -54,6 +59,51 @@ angular
     }
   ])
 
-  .controller('IndexController', ['$scope', function($scope){
+  .controller('LookController', ['$scope', '$mdSidenav', '$location', '$rootScope',
+    function($scope, $mdSidenav, $location, $rootScope){
+      $scope.look = '01';
+      $scope.loadingLook = true;
+      $scope.selecteds = ['12', '19', '22'];
+      $scope.selected = 0;
+      var i = 1;
+
+      function procuraLook(i){
+        var len = 28;
+        setTimeout(function(){
+          $rootScope.$apply(function() {
+            $scope.look = i < 10 ? '0' + i : i;
+            i++;
+            if(i <= len) procuraLook(i);
+            else {
+              $scope.loadingLook = false;
+              $scope.look = '12';
+            }
+            console.log($scope.loadingLook);
+          });
+        }, 250);
+      }
+      procuraLook(i);
+
+      $scope.next = function(){
+        $scope.selected++;
+        if($scope.selected >= $scope.selecteds.length)
+          $scope.selected = 0;
+          $scope.look = $scope.selecteds[$scope.selected];
+      }
+
+      $scope.previous = function(){
+        $scope.selected--;
+        if($scope.selected < 0)
+          $scope.selected = $scope.selecteds.length - 1;
+          $scope.look = $scope.selecteds[$scope.selected]; 
+      }
+    }
+  ])
+
+  .controller('IndexController', ['$scope', '$location', function($scope, $location){
+    
+    $scope.look = function(){
+      $location.path('/look');
+    }
 
   }])
