cd4d48ef4bf0aa07a47eddf45ec2c0b5cf7c3cd1
icaromh <icaro@icaromh.com>
2015-04-24 01:42:53 -0300

    Adicionada estrutura inicial

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,41 @@
-var app = angular.module('StarterApp', ['ngMaterial']);
+angular
+  .module('Smartlook'
+    , [ 'ngRoute'
+      , 'ngMaterial'
+    ]
+  )
 
-app.controller('AppCtrl', ['$scope', '$mdSidenav', function($scope, $mdSidenav){
-  $scope.toggleSidenav = function(menuId) {
-    $mdSidenav(menuId).toggle();
-  };
- 
-}]);
+  .config(function($routeProvider){
+
+    $routeProvider
+      
+      .when('/', {
+        controller: 'IndexController',
+        templateUrl: 'assets/templates/index.html'
+      })
+
+      .when('/login', {
+        controller: 'LoginController',
+        templateUrl: 'assets/templates/login.html'
+      })
+
+
+      .otherwise({
+        redirectTo : '/login'
+      });
+
+  })
+
+  .controller('AppCtrl', ['$scope', '$mdSidenav', '$location',
+    function($scope, $mdSidenav, $location){
+
+    }
+  ])
+
+  .controller('IndexController', ['$scope', function($scope){
+
+  }])
+
+  .controller('LoginController', ['$scope', '$mdSidenav', function($scope, $mdSidenav){
+     
+  }])
